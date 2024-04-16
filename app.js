e8958efa5c1f13cc50efe66716a0e81a51f4c9e5
anjana <bhattaanjana0@gmail.com>
2024-04-16 12:05:17 +0100

    sfwe

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
 var http = require("http");
 
-const PERSON = [{firstName: "ana", id: "1"}, {firstName: "asdf" , id: "2"}]
+const PERSON = [{ firstName: "ana", id: "1" }, { firstName: "asdf", id: "2" }]
 
-const server = http.createServer((req, res)=>{
-    if("GET" === req.method){
+const server = http.createServer((req, res) => {
+    if ("GET" === req.method) {
         res.writeHead(200, {
-            "Content-Type":"application/json"
+            "Content-Type": "application/json"
         });
         res.write(JSON.stringify(PERSON));
         res.end();
-       
+
     } else if ("POST" === req.method) {
         let body = " ";
         req.on("data", (chunk) => {
             body = body + chunk.toString();
         });
 
-        req.on("end")
-    }else if ("PUT"=== req.method){
+        req.on("end", (chunk) => {
+      
+            newObj = body.push(PERSON);
+            res.write(JSON.stringify(PERSON));
+            res.end();
+        })
+    } else if ("PUT" === req.method) {
 
-    }else if("DELETE" === req.method){
+    } else if ("DELETE" === req.method) {
 
     }
 
-    else{
+    else {
         req.writeHead(404)
         req.write("page not found")
     }
     res.end();
-       
+
 }).listen(3000);
